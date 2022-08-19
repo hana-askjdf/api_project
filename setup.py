855db1e180609d530b71453ac48bc380d3594043
hana-askjdf <55377719+hana-askjdf@users.noreply.github.com>
2022-08-19 12:22:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name ="myAPI", #언더바 하면 하이픈으로 바뀜
-    version = 0.0.1,  # relese - 엄청난 변화가 있을때 | major - 기능추가/ 몇몇 변화  | minor - 버그수정, 패키지보안할때 올림
+    version = 0.0.1,  # release - 엄청난 변화가 있을때 | major - 기능추가/ 몇몇 변화  | minor - 버그수정, 패키지보안할때 올림
     description="my api library",
     url = "https://github.com/hana-askjdf/api_project.git", # 도큐먼트나 홈페이지 링크를 주로 넣음 
     author = "chn", #개발자이름
