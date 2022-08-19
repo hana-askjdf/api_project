2eb6d397b40f1b6b806460281861c98b51519ef2
hana <sjfn242@gmail.com>
2022-08-19 10:48:24 +0900

    add : api_files

--- my_api/kakao_api.py
+++ my_api/kakao_api.py
@@ -1,17 +1,3 @@
-import requests
-def search_api(url,client_id,client_secret,params):
-    headers = 
-    {
-        "X-Naver-Client-Id" : client_id,
-        "X-Naver-Client-Secret" : client_secret
-    }
-    res = requests.get(url,params=params,headers=headers)
-    result = res.json()
-    if res.status_code != 200:
-        print(result)
-        result = None
-    return result
-
 def translate_api(text,url,client_id,client_secret,source="ko",target="en"):
     headers =  {
     "X-Naver-Client-Id" : client_id,
@@ -27,4 +13,3 @@ def translate_api(text,url,client_id,client_secret,source="ko",target="en"):
     if res.status_code == 200:
         result = result["message"]["result"]["translatedText"]
     return result
-
